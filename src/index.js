809f883e6cf8ac758362420cc80834ea16a910d6
anstss <anastasiya31017@gmail.com>
2021-02-21 23:15:03 +0200

    feat: add min

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0 ) {
+    return 0;
+  } else {
+    return Math.min(...array);
+  }
 }
 
 exports.max = function max (array) {
